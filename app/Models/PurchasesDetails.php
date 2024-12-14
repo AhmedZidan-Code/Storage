3a746ceb78a36a7521a0ac91994f2eff3020489e
omar95-pero <omarpero85@gmail.com>
2024-12-14 14:37:18 +0200

    some changes

--- app/Models/PurchasesDetails.php
+++ app/Models/PurchasesDetails.php
@@ -2,19 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PurchasesDetails extends Model
 {
     use HasFactory;
-    protected $guarded=[];
+    protected $guarded = [];
 
-    public function purchases(){
-        return $this->belongsTo(Purchases::class,'purchases_id');
+    public function purchases()
+    {
+        return $this->belongsTo(Purchases::class, 'purchases_id');
     }
 
-    public function productive(){
-        return $this->belongsTo(Productive::class,'productive_id');
+    public function productive()
+    {
+        return $this->belongsTo(Productive::class, 'productive_id');
+    }
+    /**
+     * Scope to get distinct batch numbers.
+     *
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeDistinctBatchNumbers(Builder $query)
+    {
+        return $query->select('batch_number')->distinct();
     }
 }
