0ab3a7cac33b2aeeb224338eefe0a295a0bd1145
omar95-pero <omarpero85@gmail.com>
2024-12-18 22:59:29 +0200

    edit

--- database/migrations/2024_12_18_204848_add_foreign_keys_in_rasied_ayni_table.php
+++ database/migrations/2024_12_18_204848_add_foreign_keys_in_rasied_ayni_table.php
@@ -14,11 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('rasied_ayni', function (Blueprint $table) {
-            $table->dropIndex('rasied_ayni_branch_id_foreign');
-            $table->dropIndex('rasied_ayni_storage_id_foreign');
             $table->dropIndex('rasied_ayni_productive_id_foreign');
-            $table->foreign('branch_id')->on('branches')->references('id')->cascadeOnDelete()->cascadeOnDelete();
-            $table->foreign('storage_id')->on('storages')->references('id')->cascadeOnDelete()->cascadeOnDelete();
             $table->foreign('productive_id')->on('productive')->references('id')->cascadeOnDelete()->cascadeOnDelete();
         });
     }
@@ -31,11 +27,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('rasied_ayni', function (Blueprint $table) {
-            $table->index('rasied_ayni_branch_id_foreign');
-            $table->index('rasied_ayni_storage_id_foreign');
             $table->index('rasied_ayni_productive_id_foreign');
-            $table->dropForeign('branch_id');
-            $table->dropForeign('storage_id');
             $table->dropForeign('productive_id');
         });
     }
